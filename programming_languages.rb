2bf27354607b3527ae243bbed03b7feee1e7f01f
Benjamin Coleman <hello@benjamincoleman.me>
2017-06-24 03:27:57 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,14 @@
 def reformat_languages(languages)
-  # your code here
+  reformatted_hash = {}
+  languages.each do |family, language|
+    language.each do |type, data|
+      if reformatted_hash[type]
+        reformatted_hash[type][:style].push(family)
+      else
+        reformatted_hash[type] = data
+        reformatted_hash[type][:style] = [family]
+      end
+    end
+  end
+  reformatted_hash
 end
